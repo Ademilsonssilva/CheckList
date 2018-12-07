b78313da2f4a75877bc5ed95d72c6b6552d09343
Ademilsonssilva <ademilsonssilva1@gmail.com>
2018-12-06 23:00:24 -0200

    Cadastrar usuario

---
+++ js/new_coach_user.js
@@ -0,0 +1,61 @@
+$('#cadastrar').on('click', function () {
+
+    email = $('#email').val();
+    nome = $('#nome').val();
+	pass = $('#pass').val();
+
+	if(email == '' || pass == '' || nome == '') {
+
+		swal({
+			title: 'Ops',
+			html: 'Não deixe campos em branco!',
+			type: 'warning',
+		})
+
+		
+		return;
+	}
+
+    firebase.auth().onAuthStateChanged(function(user) {
+		  
+        if (user) {
+           // User is signed in.
+           //alert('vc esta logado')
+           
+            
+           
+            //firebase.database().ref('coach/' + user.uid ).once('value').then(function(snapshot) {
+
+            firebase.auth().createUserWithEmailAndPassword(email, pass).then(function () {
+
+                firebase.database().ref('coach/' + user.uid ).set({
+                    
+                    nome: $('#nome').val(),
+                    telefone: $('#telefone').val(),
+                    email: $('#email').val(),
+
+                });
+
+                swal({
+                    html: 'Usuário cadastrado com sucesso!',
+                    type: 'success',
+                    toast: true,
+                    timer: 2500,
+                })
+
+            }).catch(function (error) {
+
+            });
+              
+            //$('#name_user').html(snapshot.val().nome);
+
+            // ...
+            //});
+
+        } else {
+           
+           window.location.href = 'login.html';
+        }
+    });
+
+});
---
+++ js/sessao.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    firebase.auth().onAuthStateChanged(function(user) {
+		  
+        if (user) {
+          
+        } else {
+         
+            window.location.href = 'login.html';
+            
+        }
+
+    });
+})
+  
+	
