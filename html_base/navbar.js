92c9e3802564c93242080265a108461e790f6011
Ademilson <ademilsonssilva1@gmail.com>
2018-12-12 09:59:30 -0200

    Includes de html base

---
+++ html_base/navbar.js
@@ -0,0 +1,31 @@
+document.write(`
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">CheckList</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="home.html">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="checklist.html">Hábitos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="new_coach_user.html">Usuários</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="new_event.html">Eventos</a>
+                    </li>
+                
+                </ul>
+            
+                <a style="color: gray" id="logout" href="#">Logout</a>
+            
+        </div>
+    </nav>
+
+`);
---
+++ js/new_event.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+
+    $('.dia').hide();
+
+    $('#todos_dias_semana').on('change', function () {
+
+        if($(this).is(':checked')) {
+
+            $('.dia').attr('checked', 'true');
+            $('.dia').hide();
+
+        }
+
+        else {
+
+            $('.dia').removeAttr('checked');
+            $('.dia').show();
+
+        }
+
+    })
+
+    $('#salvar').on('click', function () {
+
+        if($('#nome').val() == '') {
+            swal({
+                title: 'Ops',
+                html: 'Preencha o nome do evento',
+                type: 'error',
+            });
+            return false;
+        }
+
+        evento = {
+            nome: $('#nome').val(),
+            descricao: $('#descricao').val(),
+            ativo: $('#ativo').val() == 'sim' ? true : false,
+        }
+
+        dias = [];
+        $('.dia_input').each(function () {
+            if($(this).is(':checked')) {
+                dias.push($(this).prop('id'));
+            }
+        });
+
+        if(dias.length < 1) {
+            dias = ['domingo', 'segunda', 'terca', 'quarta', 'quinta', 'sexta', 'sabado'];
+        }
+
+        evento.dias = dias;
+
+        firebase.database().ref('itens').push(evento);
+
+    })
+
+    firebase.database().ref('itens').on("value", function(snapshot) {
+        dados = [];
+        snapshot.forEach(function (childSnapshot) {
+            dados.push(childSnapshot);
+        });
+
+        montaTable(dados);
+
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
+})
+
+function montaTable(array)
+{
+    $('#table_body').html('');
+
+    for(i = 0; i < array.length; i++) {
+
+        nome = array[i].val().nome;
+        key  = array[i].key;
+
+        tbody = `
+            <tr>
+                <td> ${nome} </td>
+                <td> <button class='btn btn-danger btn-sm fas fa-trash' onclick='remover_evento($(this))' ev_key='${key}'> Remover</button> </td>
+            </tr>
+        `;
+
+        $('#table_body').append(tbody);
+
+    }
+
+    if($('#table_body').html() == '') {
+        $('#table_body').append('<tr><td colspan="2"><i>Nenhuma informação encontrada!</i></td></td>');
+    }
+    
+}
+
+function remover_evento(btn)
+{
+    key = btn.attr('ev_key');
+
+    firebase.database().ref('itens/' + key).remove();
+}
