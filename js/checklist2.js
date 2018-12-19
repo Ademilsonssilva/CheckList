3a57b3498e4f372735d6d7ae148d9e149d5bbb43
Ademilson <ademilsonssilva1@gmail.com>
2018-12-19 15:14:00 -0200

    Melhorias checklist 2

--- js/checklist2.js
+++ js/checklist2.js
@@ -1,25 +1,5 @@
 $(document).ready(function () {
 
-    // function getAddress () { 
-    //     console.log('GetAddress chamada');
-    //     return new Promise ((resolve, reject)=>{ 
-    //         setTimeout(resolve, 2000); 
-    //     }); 
-    // }
-    
-    // function getUser () { 
-    //     return new Promise((resolve, reject)=>{ 
-    //         console.log('GetUser chamada');
-    //         setTimeout(_=>resolve(getAddress()), 1000); 
-    //     }); 
-    // }
-    
-    // getUser().then( result=>console.log('Done') ); 
-
-
-    // return false;
-
-
     data = moment();
 
     somador = 0;
@@ -49,13 +29,22 @@ $(document).ready(function () {
         url = `users-cliente/${logged_user}/${evento}/${ano}/${mes}/${dia}`;
 
         fdb.ref(url).set(marcado, () => {
-            mostraSemana(somador);
+            mostraSemana();
         });
         
     });
 
     function mostraSemana(somador)
     {
+
+        swal({
+            html: 'Carregando',
+            toast: true,
+            onBeforeOpen: () => {
+                Swal.showLoading();
+            }
+        })
+
         data.day(somador);
 			
         arr = [];
@@ -63,6 +52,15 @@ $(document).ready(function () {
         for( i = 0; i < 7; i++ ) {
 
             data.day(i);
+
+            if ( moment(moment().format('YYYY-MM-DD')).isSame(data.format('YYYY.MM.DD')) ) {
+
+                $('#dia-'+i).parent().css('background-color', '#DCFCDC');
+
+            }
+            else {
+                $('#dia-'+i).parent().css('background-color', '');
+            }
 				
             $('#dia-'+i).html(data.format('DD/MM'));  
 				
@@ -70,71 +68,76 @@ $(document).ready(function () {
     
         }
         
-        new Promise((resolve, reject) => {
-            resolve(carregaSemana()).then(() => {
-                alert('oi');
-            })
-        })
+        carregaSemana();
         
-
     }
 	
 	
 	function carregaSemana(){
+
+        hoje = moment();
     
-        return new Promise( (resolve, reject) => {
-
-            return fdb.ref('itens').on('value', (snapshot) => {
-                $('#tbody').html('');
-                snapshot.forEach((child) => {
-                        
-                        var trItem = $("<tr></tr>");
-                        
-                        nome = child.val().nome;
-                            
-                        tdItem = $("<td></td>");
-                        
-                        tdItem.append(nome);
-                        
-                        trItem.append(tdItem);
-                        
-                        for( i = 0; i < arr.length ; i++ ) {
-
-                            xxx = moment(arr[i].date1, 'DD/MM/YYYY');
-                            
-                            var ano = xxx.format("YYYY");
-                            var mes = xxx.format("MM");
-                            var dia = xxx.format("DD");
-                            
-
-                            gerar(child.key, dia, mes, ano);
-
-                            function gerar(evento, day, month, year) {
-
-                        
-                                fdb.ref('users-cliente/'+ logged_user+'/'+evento+'/'+ year + '/' + month + '/' + day).once("value").then(function(snapshot){
-                                    
-                                    tem = snapshot.val();
-                                    
-                                    td = $("<td class='item' dia="+day+" mes="+month+" ano="+year+" evento="+evento+"></td>");
-                                    td.append((tem == true ? "<span style='color:green' class='fas fa-check'></span>" : ""));
-                                    if(tem){
-                                        td.addClass('marcado');
-                                    }
-                                    trItem.append(td);
-                                    
-                                });
-                            
-                            }
-            
-                        }
-                        
-                        $('#tbody').append(trItem);					
+        fdb.ref('itens').once('value', (snapshot) => {
+            $('#tbody').html('');
+            snapshot.forEach((child) => {
                     
-                })
-            })
+                    var trItem = $("<tr></tr>");
+                    
+                    nome = child.val().nome;
+                        
+                    tdItem = $("<td></td>");
+                    
+                    tdItem.append(nome);
+                    
+                    trItem.append(tdItem);
+                    
+                    for( i = 0; i < arr.length ; i++ ) {
 
+                        xxx = moment(arr[i].date1, 'DD/MM/YYYY');
+                        
+                        var ano = xxx.format("YYYY");
+                        var mes = xxx.format("MM");
+                        var dia = xxx.format("DD");
+                        
+
+                        gerar(child.key, dia, mes, ano);
+
+                        function gerar(evento, day, month, year) {
+
+                    
+                            fdb.ref('users-cliente/'+ logged_user+'/'+evento+'/'+ year + '/' + month + '/' + day).once("value").then(function(snapshot){
+                                
+                                tem = snapshot.val();
+
+                                console.log(hoje);
+
+                                if(moment(hoje.format('YYYY-MM-DD')).isSame(moment(`${year}-${month}-${day}`))){
+                                    bg = '#DCFCDC';
+                                }
+                                else {
+                                    bg = '';
+                                }
+                                
+                                td = $("<td class='item' dia="+day+" mes="+month+" ano="+year+" evento="+evento+" "+ (bg != '' ? "style='background-color: "+bg : '' ) +";'></td>");
+                                td.append((tem == true ? "<span style='color:green' class='fas fa-check'></span>" : ""));
+                                if(tem){
+                                    td.addClass('marcado');
+                                }
+                                trItem.append(td);
+                                
+                            });
+                        
+                        }
+        
+                    }
+                    
+                    $('#tbody').append(trItem);					
+                
+            })
+        }).then(() => {
+            swal.close();
         });
+
     }
 
 });
